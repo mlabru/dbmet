8a39cb4e2c57fbccdbf246fe4044c9b8791ff2b8
Milton Abrunhosa <ml_byteman@yahoo.com.br>
2023-04-12 15:56:18 -0300

    load files

--- mfas/mfas_load.py
+++ mfas/mfas_load.py
@@ -82,33 +82,8 @@ def archives():
         lprc.join()
 
 # ---------------------------------------------------------------------------------------------
-def detail(f_mongo_client, fs_loc: str, flst_head: list, flst_line: list) -> None:
-    """
-    trata line
-
-    :param f_mongo_client: MongoDB client
-    :param fs_loc (str): localidade
-    :param flst_head (str): file header
-    :param fs_line (str): data register
-    """
-    # logger
-    M_LOG.info(">> detail")
-
-    # check input
-    # assert f_mongo_client
-    assert fs_loc
-    assert len(flst_head) == len(flst_line)
-
-    # create dict
-    ldct_reg = dict(zip(flst_head, flst_line))
-    M_LOG.debug("loc: %s  registro: %s", fs_loc, str(ldct_reg))
-
-    # save collection on Mongo DB
-    # db.save_data(f_mongo_client, fs_loc, ldct_reg)
-
-# ---------------------------------------------------------------------------------------------
 def data_block(f_mongo_client, f_fh: typing.TextIO,
-               fs_code: str, fi_vd_len: int, fs_date: str) -> bool:
+               fs_code: str, fi_vd_len: int, fs_date: str, fs_time: str):
     """
     process lines on file
 
@@ -116,8 +91,6 @@ def data_block(f_mongo_client, f_fh: typing.TextIO,
     :param f_fh (_io.TextIOWrapper): file handler
     :param fs_code (str): sigla da localidade
     :param fi_vd_len (int): variable definition len
-
-    :returns: error flag
     """
     # logger
     M_LOG.info(">> data_block")
@@ -128,23 +101,22 @@ def data_block(f_mongo_client, f_fh: typing.TextIO,
     assert fs_code
     assert fi_vd_len
 
+    # create registers list
+    llst_regs = []
+
     # read first detail header
     lline = f_fh.readline()
-    # logger
-    M_LOG.debug("first detail header: %s", str(lline))
 
     # detail header ?
     if not lline.startswith("#"):
         # return error
-        return False
+        return
 
     # split first detail headers
     llst_headers = lline[1:].strip().split()
 
     # read first detail line
     lline = f_fh.readline()
-    # logger
-    M_LOG.debug("first detail line: %s", str(lline))
 
     # have 2 headers ?
     if "PG" == llst_headers[0]:
@@ -156,26 +128,17 @@ def data_block(f_mongo_client, f_fh: typing.TextIO,
 
         # read second detail header
         lline = f_fh.readline()
-        # logger
-        M_LOG.debug("second detail header: %s", str(lline))
 
         # detail header ?
         if not lline.startswith("#"):
             # return error
-            return False
+            return
         
         # split second detail headers
-        llst_hdrs = lline[1:].strip().split()
-
-        # join two headers lists
-        llst_headers += llst_hdrs
-        # logger
-        M_LOG.debug("total detail header: %s", str(llst_headers))
+        llst_headers += lline[1:].strip().split()
 
         # read second detail line
         lline = f_fh.readline()
-        # logger
-        M_LOG.debug("second detail line: %s", str(lline))
 
     # senão,...
     else:
@@ -183,23 +146,28 @@ def data_block(f_mongo_client, f_fh: typing.TextIO,
         llst_detail_pg = []
         
     # process all details...
-    while lline and not lline.startswith(fs_date):
+    while lline and len(lline) > 1:  # not lline.startswith(fs_date):
         # split second detail line
-        llst_line = lline.strip().split()
-        # join two details lists
-        llst_detail = llst_detail_pg + llst_line
-        # logger
-        M_LOG.debug("total detail list: %s", str(llst_detail))
+        llst_detail = llst_detail_pg + lline.strip().split()
 
         # clear to go
         assert len(llst_headers) == len(llst_detail)
         assert len(llst_detail) == fi_vd_len + 1
 
-        # send detail to mongo
-        detail(f_mongo_client, fs_code, llst_headers, llst_detail)
-
+        # create dict
+        ldct_reg = dict(zip(llst_headers, llst_detail))
+        # append to registers list
+        llst_regs.append(ldct_reg)
+        
         # read next line
         lline = f_fh.readline()
+
+    # build data dictionary
+    ldct_data = {"datahora": f"{fs_date} {fs_time}", "vento": llst_regs}
+
+    # save collection on Mongo DB
+    # db.save_data(f_mongo_client, ldct_data, fs_code)
+    # M_LOG.debug("loc: %s  registro: %s", fs_code, str(ldct_data))
         
 # ---------------------------------------------------------------------------------------------
 def load_headers(f_mongo_client, f_fh: typing.TextIO, fs_file: str) -> None:
@@ -281,8 +249,6 @@ def load_headers(f_mongo_client, f_fh: typing.TextIO, fs_file: str) -> None:
         ls_code = llst_code[3]
         assert ls_code in df.DLST_SITES
 
-        M_LOG.debug("station code: %s", str(ls_code))
-
     # senão,...
     else:
         # logger
@@ -312,7 +278,6 @@ def load_headers(f_mongo_client, f_fh: typing.TextIO, fs_file: str) -> None:
             # date & time
             assert ls_date == llst_date[0]
             ls_time = llst_date[1]
-            M_LOG.debug("date & time: %s %s", str(ls_date), str(ls_time))
 
         # senão,...
         else:
@@ -322,7 +287,7 @@ def load_headers(f_mongo_client, f_fh: typing.TextIO, fs_file: str) -> None:
             raise Exception("invalid date data.")
 
         # process data blocks
-        data_block(f_mongo_client, f_fh, ls_code, li_vd_len, ls_date)
+        data_block(f_mongo_client, f_fh, ls_code, li_vd_len, ls_date, ls_time)
 
         # read next line
         lline = f_fh.readline()
@@ -378,8 +343,6 @@ def walk_dir(fs_dir: str) -> None:
     for lroot, ldirs, lfiles in os.walk(fs_dir, followlinks=False):
         # for all files...
         for lfile in lfiles:
-            # debug
-            M_LOG.debug("lfile: %s", str(lfile))
             # MND file ?
             if lfile.lower().endswith(".mnd"):
                 # trata file
